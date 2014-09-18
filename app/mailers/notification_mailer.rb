3d36ae0e8b960824060952a6b8a5f41935ac0167
Charles Jekal <cjekal@charless-mbp.home>
2014-09-18 00:39:13 -0400

    CJ - Stubbing out the mailer.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,3 +1,14 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "kanban@arpc.com"
+  default to: "development@arpc.com"
+
+  def no_size(cards)
+    @cards = cards
+    mail(subject: 'LeanKit: No Estimates!')
+  end
+
+  def no_shepherd(cards)
+    @cards = cards
+    mail(subject: 'LeanKit: No Shepherds!')
+  end
 end
