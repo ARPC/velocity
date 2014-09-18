5a0fa249c771b82a4bd9ed3efc0f826a2edf72e9
Charles Jekal <cjekal@charless-mbp.home>
2014-09-17 21:11:52 -0400

    CJ - Mimicking real LeanKitKanban behavior.

--- lib/kanban/api.rb
+++ lib/kanban/api.rb
@@ -4,7 +4,7 @@ require 'kanban/card'
 module Kanban
   class Api
     def self.done_cards
-      board = LeanKitKanban::Board.find(46341228)
+      board = LeanKitKanban::Board.find(46341228).first
       done_lane = board['Lanes'].select {|lane| lane['Title'] == 'Done'}.first
       return [] if done_lane.nil?
       done_lane['Cards'].map {|card| Card.new(card) }
--- spec/lib/kanban/api_spec.rb
+++ spec/lib/kanban/api_spec.rb
@@ -9,7 +9,7 @@ describe Kanban::Api do
     end
 
     it 'returns the cards' do
-      expect(LeanKitKanban::Board).to receive(:find).with(46341228).and_return(@lkk_response)
+      expect(LeanKitKanban::Board).to receive(:find).with(46341228).and_return([@lkk_response])
       cards = Kanban::Api.done_cards
       expect(cards.length).to eq(2)
       cards.each {|card| expect(card).to be_a(Kanban::Card) }
@@ -17,7 +17,7 @@ describe Kanban::Api do
 
     it 'only returns the done cards' do
       @lkk_response['Lanes'].delete(@done_cards)
-      expect(LeanKitKanban::Board).to receive(:find).with(46341228).and_return(@lkk_response)
+      expect(LeanKitKanban::Board).to receive(:find).with(46341228).and_return([@lkk_response])
       expect(Kanban::Card).not_to receive(:new).with(any_args)
       expect(Kanban::Api.done_cards).to eq([])
     end
