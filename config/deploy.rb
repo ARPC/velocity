f590e63449eaddfc4791af8231e59dcf90f70724
Charles Jekal <cjekal@Charless-MacBook-Pro.local>
2014-09-24 08:32:44 -0400

    CJ - Adding secrets.yml to linked-files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/home/deploy/velocity'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/initializers/lean_kit_kanban.rb}
+set :linked_files, %w{config/database.yml config/secrets.yml config/initializers/lean_kit_kanban.rb}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
