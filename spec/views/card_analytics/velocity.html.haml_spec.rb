9fa88206ddc33e647be232bf57db60cd7437c46f
Pair1 <pair1@client3.arpcdev.local>
2015-05-27 17:43:54 -0400

    ZMCJ - Rounding the velocity information to two decimal places.

--- spec/views/card_analytics/velocity.html.haml_spec.rb
+++ spec/views/card_analytics/velocity.html.haml_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "card_analytics/velocity.html.haml", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'rounds velocities to two decimal places' do
+    assign(:last_4_avg_velocity, 5.5555555555)
+    assign(:avg_velocity, 6.66666666)
+
+    render
+
+    expect(rendered).to match(/5\.56/)
+    expect(rendered).to match(/6\.67/)
+  end
 end
